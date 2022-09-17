bec3170ca174fd7a716646ec7b32a05f52992bbe
Karácsony Márton <41687291+KaracsonyMarton@users.noreply.github.com>
2022-09-17 11:18:39 +0200

    [update] vite replaced with webpack

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,6 @@
-import './bootstrap';
-
-import Alpine from 'alpinejs';
-
-window.Alpine = Alpine;
-
-Alpine.start();
+require('./bootstrap');
+window.Vue = require('vue').default;
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+const app = new Vue({
+    el: '#app'
+});
